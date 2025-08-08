cac175cf9672858cde543abd1b50b1874e466909
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-08-08 16:27:57 +0530

    java Bike Programm

---
+++ Bike.java
@@ -0,0 +1,15 @@
+class Bike{
+	public static void main(String [] args){
+		int sparkPlug=150;
+        int enginOil=800;
+        int battery=2000;
+        int tyre=2200;
+        int seatCover=600;
+    System.out.println("Spark Plug=="+sparkPlug);
+    System.out.println("Engin oil=="+enginOil);
+    System.out.println("Battery"+battery);
+    System.out.println("Tyre=="+tyre);
+    System.out.println("Seat Cover=="+seatCover);	
+		
+	}
+}
