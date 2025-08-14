c6b28a97c137f2ebbb4bc98e71d87eb166b543f7
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-08-14 15:50:52 +0530

    IF-Condition

---
+++ IF-condition/Birds.java
@@ -0,0 +1,77 @@
+class Birds{
+	public static int andraState(double num){
+		System.out.println("____________details of some Bird species in India____________");
+				if(num==1)
+			System.out.println("Andra Pradesh");
+			int Num=54;
+			return Num;
+	}
+	public static int karnatakaState(double code){
+	    if(code==2)
+			System.out.println("Karnataka");
+		int num=428;
+		return num;
+			
+	}
+	public static int keralaState(double number){
+		if(number==3)
+			System.out.println("Kerala");
+		int num=356;
+		return num;
+	}
+	public static int tamilState(double total){
+		if(total==4)
+			System.out.println("Tamil Nadu");
+		int num=402;
+		return num;
+	}
+	public static int maharastraState(double maharastra){
+		if(maharastra==5)
+			System.out.println("Maharastra");
+		int num=472;
+		return num;
+	}
+	public static int gujaratState(int gujarat){
+		if(gujarat==6)
+			System.out.println("gujarat");
+		int num=365;
+		return num;
+	}
+     
+    public static int rajastanState(int rajastan){
+		if(rajastan==7)
+			System.out.println("Rajastan");
+		int num=365;
+		return num;
+	}	
+
+    public static int panjabState(double panjab){
+		if(panjab==8)
+			System.out.println("Panjab");
+		int num=276;
+		return num;
+	}		 	
+	
+	 public static int westengalState(double west){
+		if(west==9)
+			System.out.println("West Bengal");
+		int num=412;
+		return num;
+	}		 	
+	
+	public static int assamState(double assam){
+		if(assam==10)
+			System.out.println("Assam");
+		int num=452;
+		return num;
+	}		 	
+	
+	public static int odishaState(double odisha){
+		if(odisha==11)
+			System.out.println("Odisha");
+		int num=452;
+		return num;
+	}		 	
+	
+}
+	
---
+++ IF-condition/Country.java
@@ -0,0 +1,127 @@
+class Country{
+	
+	public static double indiaPeople(int ind){
+		System.out.println("________Some countrys population in millions format_______");
+		if(ind==1)
+			System.out.println("India Population");
+		double total=1408.12345d;
+			return total;
+		
+	}
+	
+	public static double chinaPeople(int china){
+		if(china==2)
+			System.out.println("china Population");
+		double total=1409.5467d;
+			return total;
+		}
+
+    public static double usPeople(int us){
+		if(us==3)
+			System.out.println("United States peoples");
+		double total=339.9909d;
+		return total;
+	}                
+	
+	public static double indonesiaPeople(int indo){
+		if(indo==4)
+			System.out.println("Indonesia peoples");
+		double total=227.02987d;
+		return total;
+			
+	}
+	
+	public static double pakistanPeople(int pak){
+		if(pak==5)
+			System.out.println("Pakistan Peoples");
+		double total=216.5765d;
+		return total;
+	}
+	
+	public static double nigeriaPeople(int nigeria){
+		if(nigeria==6)
+			System.out.println("Nigeria Peoples");
+		double total=203.4565d;
+		return total;
+	}
+	
+	public static double brazilPeople(int brazil){
+		if(brazil==7)
+			System.out.println("Brazil peoples");
+		double total=172.5673d;
+		return total;
+		}
+	
+	public static double russiaPeople(int russia){
+		if(russia==8)
+			System.out.println("Russia Peoples");
+		double total=144.454d;
+		return total;
+	}	
+	
+	public static double mexicoPeople(double mex){
+		if(mex==9)
+			System.out.println("Mexico Peoples");
+		double total=128.85d;
+		return total;
+	}
+	
+	public static double japanPeople(double japan){
+		if(japan==10)
+			System.out.println("Japan Peoples");
+		double total=125.123d;
+		return total;
+	}
+	
+	public static double ethiopiaPeople(double ethiopia){
+		if(ethiopia==11)
+			System.out.println("Ethiopia Peoples");
+		double total=115.768d;
+		return total;
+	}
+	
+	public static double philippinesPeople(double philippin){
+		if(philippin==12)
+			System.out.println("Philippines Peoples");
+		double total=128.85d;
+		return total;
+	}
+	
+	public static double egyptPeople(double egypt){
+		if(egypt==13)
+			System.out.println("Egypt Peoples");
+		double total=102.87d;
+		return total;
+	}
+	
+	public static double dr_CongoPeople(int congo){
+		if(congo==14)
+			System.out.println("DR Congo Peoples");
+		double total=98.123d;
+		return total;
+	}
+	
+	public static double vietnamPeople(int vietnam){
+		if(vietnam==15)
+			System.out.println("Vietnam Peoples");
+		double total=128.85d;
+		return total;
+	}
+	
+	public static double iranPeople(int iran){
+		if(iran==16)
+			System.out.println("Iran Peoples");
+		double total=128.85d;
+		return total;
+	}
+	
+	public static double bangladeshPeople(int bangla){
+		if(bangla==17)
+			System.out.println("Bangladesh Peoples");
+		double total=169.65d;
+		return total;
+	}
+	
+	
+	
+}
