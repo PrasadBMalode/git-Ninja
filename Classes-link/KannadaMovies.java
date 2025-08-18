8943f911ffaeb9a485df4bd9348a56a81e94cc6d
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-08-18 11:40:54 +0530

    Classes-link

---
+++ Classes-link/KannadaMovies.java
@@ -0,0 +1,25 @@
+class KannadaMovies{
+	
+	public static void suspensMovie(){
+		
+				System.out.println("in KFI Rangitaranga One of the best suspense movie");
+		
+		EnglishMovies.scifiMovie();
+	}
+	
+	public static void loveMovie(){
+		
+		        System.out.println("in KFI 99 is a love zoner movie");
+		KannadaMovies.suspensMovie();
+	}
+	
+	public static void comedyMovie(){
+		        System.out.println("Entering in the methods");
+		System.out.println("------------------------------------------------------");
+		
+		        System.out.println("in KFI Victory 2 is one of a comedy movie");
+		KannadaMovies.loveMovie();
+		
+	}
+	
+}
