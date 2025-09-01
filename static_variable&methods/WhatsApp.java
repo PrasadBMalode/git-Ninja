500d094eaa846bf979fdf6e60cb3cfe584530d71
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-09-01 23:43:16 +0530

    static_variable&methods

---
+++ static_variable&methods/WhatsApp.java
@@ -0,0 +1,12 @@
+class WhatsApp{
+	public WhatsApp(){
+		System.out.println("its a default constructor");
+	}
+	static String name="prasad"; 
+	
+	public static void prasad(long number){
+		System.out.println("its prasad method and his number is"+number);
+		
+	}
+	
+}
---
+++ static_variable&methods/WhatsAppRunner.java
@@ -0,0 +1,7 @@
+class WhatsAppRunner{
+	public static void main(String [] args){
+		new WhatsApp();
+		System.out.println(WhatsApp.name);
+		WhatsApp.prasad(6360211265l);
+	}
+}
