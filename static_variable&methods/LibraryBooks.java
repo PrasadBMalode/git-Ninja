a3ce791b9963c9301b19dbb0e9b0b17577e26db7
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-09-01 18:59:02 +0530

    static_variable&methods

---
+++ static_variable&methods/LibraryBooks.java
@@ -0,0 +1,18 @@
+class LibraryBooks{
+	public LibraryBooks(){
+		System.out.println("its a default constructor");
+	}
+	static int amount=1800;
+	
+	static void name(String title,int cost, char book_code, boolean happened){
+		System.out.println(title);
+		
+		System.out.println(cost);
+		
+		System.out.println(book_code);
+		
+		System.out.println(happened);
+		
+	}
+	
+}
---
+++ static_variable&methods/LibraryBooksRunner.java
@@ -0,0 +1,11 @@
+class LibraryBooksRunner{
+	public static void main(String args []){
+		LibraryBooks librarybooks=new LibraryBooks();
+		System.out.println(LibraryBooks.amount);
+		System.out.println("Harry_Potter");
+		System.out.println(2300);
+		System.out.println('H');
+		System.out.println(false);
+	 
+	}
+}
