79ec2e2d65d6bd548ce5df187a3e3b798154784d
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-09-19 12:14:46 +0530

    One-To-Many Association

---
+++ One-To-Many Association/Garden - Copy.java
@@ -0,0 +1,15 @@
+class Garden{
+	public String gardenName;
+	public Water water;
+	
+	public Garden(String gardenName,Water water){
+		this.gardenName=gardenName;
+		this.water=water;
+	}
+	public void gardenDisplay(){
+		System.out.println("Garden name=="+gardenName);
+		System.out.println(water.leterOfWater);
+		System.out.println(water.isFilteredWater);
+		
+	}
+}
