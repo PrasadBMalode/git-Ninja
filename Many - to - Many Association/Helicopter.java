708966602c402672b3ad9f20c777fb6f9a36ea72
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-09-21 00:06:47 +0530

    Many - to - Many Association

---
+++ Many - to - Many Association/Helicopter.java
@@ -0,0 +1,10 @@
+class Helicopter{
+	public String firstHelicopterName;
+	public String secondHelicopterName;
+	
+	public Helicopter(String firstHelicopterName,String secondHelicopterName){
+		this.firstHelicopterName=firstHelicopterName;
+		this.secondHelicopterName=secondHelicopterName;
+	}
+	
+}
