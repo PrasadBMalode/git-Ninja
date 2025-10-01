7c71cf7e75f7cd412853ff79709cebe1bc3fe4db
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-10-01 22:36:32 +0530

    (Array) recape 2nd time

--- (Array) recape2/BuildingRunner.java
+++ (Array) recape2/BuildingRunner.java
@@ -3,11 +3,17 @@
 
         Building building = new Building();
 		
-        Room[] roomsFloor1 = { new Room("Fan", "Light") };
-        Room[] roomsFloor2 = { new Room("Chair", "Table") };
-        Room[] roomsFloor3 = { new Room("Sofa", "TV") };
-        Room[] roomsFloor4 = { new Room("Frames", "Fridge") };
-        Room[] roomsFloor5 = { new Room("DiningTable", "BedLamp") };
+        Room[] roomsFloor1 = { new Room("Fan", "Light","Fridg","bedlamp","mike") };
+        Room[] roomsFloor2 = { new Room("Chair", "Table","speekrs","battub","jug") };
+        Room[] roomsFloor3 = { new Room("Sofa", "TV","slippers","shoes","dress") };
+        Room[] roomsFloor4 = { new Room("Frames", "Fridge","bottle","pens","stickers") };
+        Room[] roomsFloor5 = { new Room("DiningTable", "BedLamp","remote","shelph","box") };
+		Room[] roomsFloor6 = { new Room("doll", "windo","knife","cutter","wires") };
+		Room[] roomsFloor7 = { new Room("books", "doumble","Fruits","Ac","bulb") };
+		Room[] roomsFloor8 = { new Room("dore", "walpaper","spects","camara","Photos") };
+		Room[] roomsFloor9 = { new Room("coile", "switchs","shoeRack","socks","Keys") };
+		Room[] roomsFloor10 = { new Room("ac", "heater","mackupset","charger","phone") };
+		
 
         
         Floor firstFloor = new Floor("SSG", 101, roomsFloor1);
@@ -15,9 +21,15 @@
         Floor thirdFloor = new Floor("FFG", 103, roomsFloor3);
         Floor fourthFloor = new Floor("HHG", 104, roomsFloor4);
         Floor fifthFloor = new Floor("JJG", 105, roomsFloor5);
+		Floor sixthFloor = new Floor("KKG", 106, roomsFloor6);
+		Floor seventhFloor = new Floor("LLG", 107, roomsFloor7);
+		Floor eighthFloor = new Floor("PPG", 108, roomsFloor8);
+		Floor ninthFloor = new Floor("OOG", 109, roomsFloor9);
+		Floor tenthFloor = new Floor("IIG", 110, roomsFloor10);
 
         
-        Floor[] floors = { firstFloor, secondFloor, thirdFloor, fourthFloor, fifthFloor };
+        Floor[] floors = { firstFloor, secondFloor, thirdFloor, fourthFloor, fifthFloor,
+		                   sixthFloor,seventhFloor, eighthFloor,ninthFloor,tenthFloor};
 
         
         building.displayBuilding(floors);
--- (Array) recape2/Room.java
+++ (Array) recape2/Room.java
@@ -1,14 +1,25 @@
 class Room {
     public String roomFirstProperty;
     public String roomSecondProperty;
+	public String roomThirdProperty;
+	public String roomFourthProperty;
+	public String roomFifthProperty;
 
-    public Room(String roomFirstProperty, String roomSecondProperty) {
+    public Room(String roomFirstProperty, String roomSecondProperty, String roomThirdProperty,
+	                 String roomFourthProperty, String roomFifthProperty) {
         this.roomFirstProperty = roomFirstProperty;
         this.roomSecondProperty = roomSecondProperty;
+		this.roomThirdProperty=roomThirdProperty;
+		this.roomFourthProperty=roomFourthProperty;
+		this.roomFifthProperty=roomFifthProperty;
     }
 
     public void displayRoom() {
         System.out.println("First property inside room: " + roomFirstProperty);
         System.out.println("Second property inside room: " + roomSecondProperty);
+		System.out.println("Third property inside room: " + roomThirdProperty);
+		System.out.println("Fourth property inside room: " + roomFourthProperty);
+		System.out.println("Fifth property inside room: " + roomFifthProperty);
+		
     }
 }
