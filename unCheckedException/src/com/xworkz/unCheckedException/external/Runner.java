3fad6d8ea666b66324f8f7c886f579ec17a2b22a
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-11-17 22:12:42 +0530

    UnChecked Exception

---
+++ unCheckedException/src/com/xworkz/unCheckedException/external/Runner.java
@@ -0,0 +1,61 @@
+package com.xworkz.unCheckedException.external;
+
+import com.xworkz.unCheckedException.event.UnCheckExceptions;
+
+public class Runner {
+    public static void main(String[] args) {
+        UnCheckExceptions obj=new UnCheckExceptions();
+
+        obj.annotationTypeMissmatchException();
+        obj.arithmeticException();
+        obj.arrayStoreException();
+        obj.bufferOverflowException();
+        obj.bufferUnderFlowException();
+        obj.cannotRedoException();
+        obj.cannotUndoException();
+        obj.classCastException();
+        obj.cmmException();
+        obj.completionException();
+        obj.concurrentModificationException();
+        obj.dateTimeException();
+        obj.domException();
+        obj.emptyStackException();
+        obj.enumConstantNotPresentException();
+        obj.eventException();
+        obj.fileSystemAlreadyExistsException();
+        obj.fileSystemNotFoundException();
+        obj.illegalArgumentException();
+        obj.illegalMonitorStateException();
+        obj.illegalPathStateException();
+        obj.illegalStateException();
+        obj.illformedLocaleException();
+        obj.imagingOpException();
+        obj.incompleteAnnotationException();
+        obj.indexOutOfBoundsException();
+        obj.jmRuntimeException();
+        obj.lsException();
+        obj.malformedParameterizedTypeException();
+        obj.malformedParametersException();
+        obj.mirroredTypesException();
+        obj.mirroredTypeException();
+        obj.missingResourceException();
+        obj.negativeArraySizeException();
+        obj.noSuchElementException();
+        obj.noSuchMechanismException();
+        obj.nullPointerException();
+        obj.profileDataException();
+        obj.providerException();
+        obj.providerNotFoundException();
+        obj.rasterFormatException();
+        obj.rejectedExecutionException();
+        obj.securityException();
+        obj.typeConstraintException();
+        obj.typeNotPresentException();
+        obj.uncheckedIOException();
+        obj.undeclaredThrowableException();
+        obj.unmodifiableSetException();
+        obj.unsupportedOperationException();
+        obj.webServiceException();
+        obj.wrongMethodTypeException();
+    }
+}
