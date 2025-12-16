533c38ec976156d926e13346c8a60ed66297fb1d
PrasadBMalode <prasadsaix-workz@outlook.com>
2025-12-16 11:49:14 +0530

    variables initilization (Litteral, reference, method, constructor)

---
+++ VarInitilization/src/com/xworkz/VarInitilization/outer/VarInitilization.java
@@ -0,0 +1,25 @@
+package com.xworkz.VarInitilization.outer;
+
+public class VarInitilization {
+    int a;
+    VarInitilization(int a) {
+            this.a = a;
+        }
+
+        static int getValue() {
+            return 30;
+        }
+
+        public static void main(String[] args) {
+
+            int a = 10;
+            int b = a;
+            int c = getValue();
+            VarInitilization obj = new VarInitilization(50);
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(obj.a);
+        }
+    }
